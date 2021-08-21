06e5f14496f24113946c5f6a696c438a314bf1a0
Uma Thileepan <thileepan_uma@yahoo.co.uk>
2021-08-21 19:27:19 +0100

    Union: combine multiple statements with different conditions.

--- joiningFromMultiplePlaces.sql
+++ joiningFromMultiplePlaces.sql
@@ -112,3 +112,25 @@ ORDER BY first_name;
 
 SELECT *
 FROM products, shippers;
+
+-- Union combine multiple statements 
+SELECT order_id, order_date, 'Active' AS status
+FROM orders
+WHERE order_date >= '2019-01-01'
+UNION
+SELECT order_id, order_date, 'Archived' AS status
+FROM orders
+WHERE order_date <= '2019-01-01';
+
+SELECT customer_id, first_name, points, 'Bronze' AS type
+FROM customers
+WHERE points < 2000
+UNION
+SELECT customer_id, first_name, points, 'Silver' AS type
+FROM customers
+WHERE points BETWEEN 2000 AND 3000
+UNION
+SELECT customer_id, first_name, points, 'Gold' AS type
+FROM customers
+WHERE points BETWEEN 3000 AND 5000
+ORDER BY first_name;
