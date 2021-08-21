07371529e8a154b824a1d82d8db238d917e89c14
Uma Thileepan <thileepan_uma@yahoo.co.uk>
2021-08-21 15:57:36 +0100

    Create joiningFromMultiplePlaces.sql
    Join multiple table using JOIN ON.

---
+++ joiningFromMultiplePlaces.sql
@@ -0,0 +1,16 @@
+Use sql_store;
+
+SELECT order_id, o.customer_id, first_name, last_name 
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id; 
+    
+SELECT order_id, oi.product_id, quantity, oi.unit_price
+FROM order_items oi
+JOIN products p 
+	ON oi.product_id = p.product_id;
+    
+SELECT *
+FROM order_items oi
+JOIN sql_inventory.products p
+	ON oi.product_id = p.product_id;
