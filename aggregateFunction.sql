33964d998b4e4882f689bfeb4ea7dc28f463bff9
Uma Thileepan <thileepan_uma@yahoo.co.uk>
2021-08-22 09:10:10 +0100

    WITH ROLLUP
    WITH ROLLUP will summerise data.

--- aggregateFunction.sql
+++ aggregateFunction.sql
@@ -91,3 +91,17 @@ WHERE state = 'VA'
 GROUP BY c.customer_id, c.first_name, c.last_name
 HAVING total_sales > 100;
 
+-- WITH ROLLUP : For summerizing data
+SELECT 
+	client_id,
+    SUM(invoice_total) AS total_sales
+FROM invoices i 
+GROUP BY client_id WITH ROLLUP;
+
+SELECT 
+	pm.name AS payment_method,
+    SUM(amount) AS total
+FROM payments p 
+JOIN payment_methods pm
+	ON p.payment_method = pm.payment_method_id
+GROUP BY payment_method WITH ROLLUP;
