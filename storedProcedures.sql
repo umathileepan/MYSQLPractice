b235d9e181be5773036dc5c64183d97e23f4ee89
Uma Thileepan <thileepan_uma@yahoo.co.uk>
2021-08-23 08:24:00 +0100

    Stored procedure with Validation

--- storedProcedures.sql
+++ storedProcedures.sql
@@ -42,7 +42,7 @@ BEGIN
 END$$
 DELIMITER ;
 
-call get_payments(5,1);
+call get_payments(null,1);
 
 -- Mosh's solution for the above requirement
 DROP PROCEDURE IF EXISTS get_payments;
@@ -52,5 +52,20 @@ CREATE PROCEDURE get_payments(client_id INT, payment_method_id TINYINT)
 BEGIN
 	SELECT * FROM payments p
     WHERE p.client_id = IFNULL(client_id, p.client_id) AND p.payment_method = IFNULL(payment_method_id, p.payment_method);
+END$$
+DELIMITER ;
+
+-- Validation
+DELIMITER $$
+CREATE PROCEDURE `make_payment`(invoice_id INT, payment_amount DECIMAL(9,2), payment_date DATE)
+BEGIN
+	IF payment_amount <= 0 THEN
+		SIGNAL SQLSTATE '22003' SET MESSAGE_TEXT = 'Invalid payment amount';
+	END IF;
+	UPDATE invoices i 
+    SET
+		i.payment_total = payment_amount,
+        i.payment_date = payment_date
+	WHERE i.invoice_id = invoice_id;
 END$$
 DELIMITER ;
