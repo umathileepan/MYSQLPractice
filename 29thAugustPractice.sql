b2747d53d509a385cf5cce5346f47c4ce3e2636a
Uma Thileepan <thileepan_uma@yahoo.co.uk>
2021-08-29 14:04:29 +0100

    Update 29thAugustPractice.sql

--- 29thAugustPractice.sql
+++ 29thAugustPractice.sql
@@ -16,3 +16,53 @@ SELECT * FROM customers  WHERE NOT (birth_date > '1990-01-01' OR points > 1000 A
 SELECT * FROM orders  WHERE order_date < now();
 
 SELECT order_id, unit_price * quantity AS total_price FROM order_items WHERE order_id = 6 AND (unit_price * quantity) >30;
+
+SELECT * FROM customers WHERE state IN('CA','GA','FL');
+
+SELECT * FROM customers WHERE state NOT IN('CA','GA','FL');
+
+SELECT * FROM products WHERE quantity_in_stock IN('49','38','72');
+
+SELECT * FROM products WHERE quantity_in_stock BETWEEN 30 AND 90;
+
+SELECT * FROM customers WHERE birth_date BETWEEN '1990-01-01' AND '2000-01-01';
+
+SELECT * FROM customers WHERE last_name LIKE 'b%ey';
+
+SELECT * FROM customers WHERE last_name LIKE '_____y';
+
+SELECT * FROM customers WHERE address LIKE '%trail%' OR address LIKE '%avenue%';
+ 
+SELECT * FROM customers WHERE phone LIKE '%9' ;
+
+SELECT * FROM customers WHERE address REGEXP 'trail$';
+
+SELECT * FROM customers WHERE first_name REGEXP '^mer|Shel|Erich';
+
+SELECT * FROM customers WHERE last_name REGEXP '[abd]e';
+
+SELECT * FROM customers WHERE first_name REGEXP 'elka|amber';
+
+SELECT * FROM customers WHERE last_name REGEXP 'EY$|ON$';
+
+SELECT * FROM customers WHERE last_name REGEXP '^MY|SE';
+
+SELECT * FROM customers WHERE last_name REGEXP 'B[RU]';
+
+SELECT * FROM customers WHERE phone IS NULL;
+
+SELECT * FROM customers WHERE phone IS NOT NULL;
+
+SELECT * FROM orders WHERE shipped_date IS NULL; 
+
+SELECT * FROM customers ORDER BY first_name DESC, state DESC;
+
+SELECT *, (unit_price * quantity) AS total_price FROM order_items WHERE order_id = 2 ORDER BY (unit_price * quantity) DESC;
+
+SELECT *, (unit_price * quantity) AS total_price FROM order_items WHERE order_id = 2 ORDER BY total_price DESC;
+
+SELECT * FROM customers LIMIT 10;
+
+SELECT * FROM customers LIMIT 6,10;
+
+SELECT * FROM customers ORDER BY points DESC LIMIT 3;
